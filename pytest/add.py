b5985f20637333ee5e610b7f3255797f95da7510
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2021-05-03 21:08:52 +0530

    Modified the files

--- pytest/add.py
+++ pytest/add.py
@@ -1,1 +1,18 @@
+class Lowbalance(Exception):
+    pass
 
+class Amount(object):
+    def __init__(self, initialvalue=0):
+        self.balance = initialvalue
+
+    def spend_cash(self, amount):
+        if self.balance < amount:
+            raise Lowbalance('{}'.format(amount))
+        self.balance -= amount
+
+    def add_cah(self,amount):
+        self.balance += amount
+
+
+def add(x,y):
+    return (x+y)
